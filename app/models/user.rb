41bf4749b942d773cee33df5656800ac95af9511
AnishaZ <anishagzacharia@gmail.com>
2017-05-02 18:28:14 -0700

    Added validation to date of birth in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ActiveRecord::Base
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :name, :alias, presence: true, length: { in: 2..40 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
-  validates :date_of_birth, presence: true
+  validates :date_of_birth, presence: true #still keep this in to check for presence
+  validate :date_of_birth_cannot_be_in_the_future #custom validations dont have a plural validates
+
+  def date_of_birth_cannot_be_in_the_future
+    if date_of_birth > Date.today
+      errors.add(:date_of_birth, "can't be in the future")
+    end
+  end
 
   before_save :lowercase
 
